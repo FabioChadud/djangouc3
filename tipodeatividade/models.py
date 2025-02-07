0b1676b3472ce874c41cae98edaa64b1c510c2ec
Fabio <fabiochd@yahoo.com.br>
2025-02-07 19:45:20 -0300

    wdwq

--- tipodeatividade/models.py
+++ tipodeatividade/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 class TipoDeAtividade(models.Model):
     
-    codigo = models.CharField(primary_key=True, max_length=100,
+    codigo = models.AutoField(primary_key=True, 
                               help_text='Código do Tipo de atividade')
     descricao = models.CharField(max_length=70, null=False,
                                  help_text='Informe a descrição do Tipo de Atividade')
