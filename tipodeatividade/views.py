6d2ff5c74fcd1d09381c7fa2f93879cd1ac84fcb
Fabio <fabiochd@yahoo.com.br>
2025-02-21 21:43:20 -0300

    a

--- tipodeatividade/views.py
+++ tipodeatividade/views.py
@@ -9,21 +9,13 @@ def index(request):
 
 
 def listar(request):
-    lista_tipodeatividade = TipoDeAtividade.objects.order_by("descricao")    
-    resposta = "<ul>"
-    for tipodeatividade in lista_tipodeatividade:
-        resposta += '<li>{0}</li>'.format(tipodeatividade.descricao)
+    lista_tipodeatividade = TipoDeAtividade.objects.all()    
+    contexto= {
+        'tiposdeatividades': lista_tipodeatividade,
+    }
     
-    resposta += '</ul>'
-    print(resposta)
-    return HttpResponse(resposta)
-
+    return render(request, 'tipodeatividade/listarTipoDeAtividade.html', context=contexto)
     
-def show_mensagem(request):
-    x = "M"
-    nome = x + "arcos, tudo bem?"
-    return HttpResponse(f"Bom dia!{nome}")   
-
 def detalhe_tipodeatividade(request, ta_codigo):
     tipodeatividade = TipoDeAtividade.objects.get(pk=ta_codigo)
     return HttpResponse(tipodeatividade)
--- titulo/views.py
+++ titulo/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from titulo.models import Titulo
+
 # Create your views here.
+
 def listar(request):
-    return HttpResponse("Lista de Títulos")
+    lista_titulo = Titulo.objects.all()    
+    contexto= {
+        'titulo': lista_titulo,
+    }
+    
+    return render(request, 'titulo/listarTitulo.html', context=contexto)
+    
+def detalhe_titulo(request, ti_codigo):
+    titulo = Titulo.objects.get(pk=ti_codigo)
+    return HttpResponse(titulo)
